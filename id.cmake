9a603fd7fdc2b2d1cbd9f6a096f7935d38464135
David Callu <callu.david@gmail.com>
2017-11-03 23:41:27 +0100

    feat(cmake): add version 0.7.2-unowhy-1

---
+++ id.cmake
@@ -0,0 +1,14 @@
+set(package QuaZip)
+set(version 0.7.2-unowhy)
+
+byd__package__info(${package}
+    MAINTAINER_NAME "David Callu"
+    MAINTAINER_EMAIL "callu.david@gmail.com"
+    VERSION ${version}-1
+    ABI ${version}
+    )
+
+byd__package__download_info(${package}
+    URL "https://github.com/ledocc/QuaZip/archive/v0.7.2-unowhy.tar.gz"
+    URL_HASH SHA1=429794c276fb4458c2123356969609932875c396
+    )
